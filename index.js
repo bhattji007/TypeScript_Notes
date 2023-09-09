2d9dad305e4d1e391ae7e3f57255ea028a921919
shubham <shubham@saasden.club>
2023-09-09 07:23:42 +0000

    Day 1

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+// Typescript is a "Type Saftey" version of javascript and is mainly used to idemtofy error before the runtime 
+// JS does not provide static checking but TS does provide it 
+// The only job of TS is to analyze the code as we type it .
+// DEVELOPMENT PROCESS
+// TypeScript code is transpiled to JavaScript Code 
+// INTRO
+const user = { name: "shubham", age: 19 };
+console.log("shubham bhatt", user.email);
---
+++ index.ts
@@ -0,0 +1,20 @@
+// Typescript is a "Type Saftey" version of javascript and is mainly used to idemtofy error before the runtime 
+// JS does not provide static checking but TS does provide it 
+// The only job of TS is to analyze the code as we type it .
+// DEVELOPMENT PROCESS
+// TypeScript code is transpiled to JavaScript Code 
+
+
+// INTRO
+
+const  user= {name:"shubham", age:19}
+console.log("shubham bhatt", user.name);
+
+// situations where TS is helpful
+
+// ex- a function accepting 2 numbers :- don't need to explicity test
+
+// ex- a function returning a string 
+
+// declaring a variable 
+
